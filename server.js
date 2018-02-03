f22897b43128cd5560adf22de5fa28df7618bed8
Alan Drake <alangeorgedrake@gmail.com>
2018-02-03 11:11:19 -0700

    adding in tentative server

---
+++ server.js
@@ -0,0 +1,27 @@
+//Dependencies used
+
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+//Setting up basic config for express
+
+var app = express();
+
+//Setting up initial port
+
+var PORT = process.env.PORT || 3000;
+
+//sets up express to handle data parsing
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Handles routes.  Will change later
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+//Listener to "start" server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+})
