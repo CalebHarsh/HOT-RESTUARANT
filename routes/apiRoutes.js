a50226f7bfa2ad2974c1b58695638b4aff483448
Dillon Schultz <dillonschultz93@gmail.com>
2018-02-03 12:21:29 -0700

    Made JSON GET functions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,13 @@
-const reservations = require('../data/reservationsData.js')
+var reservations = require('../data/reservationsData.js');
+var tables = require('../data/tablesData.js');
 
 module.exports = function(app) {
-  app.get("/api/reservations", function(req, res) {
-    res.json(reservations);
-  });
+  //reservation JSON
+  app.get('/api/reservations', function(req, res){
+    res.json(reservations)
+  })
+  //tables JSON
+  app.get('/api/tables', function(req, res){
+    res.json(tables)
+  })
 }
